87a45362c303f9f089d442b4eb4ec672d1e15ff8
Muhammad-Sharif Moustafa <sharif@Muhammad-Sharifs-MacBook-Pro.local>
2014-02-21 15:00:55 -0500

    Initial Commit

---
+++ WorkoutLog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WorkoutLog
+//
+//  Created by Muhammad-Sharif Moustafa on 2/21/14.
+//  Copyright (c) 2014 Muhammad-Sharif Moustafa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
